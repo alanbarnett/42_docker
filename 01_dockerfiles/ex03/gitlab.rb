6d7d6dc5b6d6d4d0f754c4c591f820cbb2b151fb
Alan Barnett <alanbarnett328@gmail.com>
2019-08-03 07:53:43 -0700

    feat(https): working https!
    Some things had to change. The most important change, I believe, was
    changing the external url from gitlab.abarnett.com to the ip of the
    docker machine. Since I couldn't do any dns configuration,
    gitlab.abarnett.com never led to anything. I thought I needed the port
    in the external url, but it didn't work when I had that in. I've also
    changed the location of the ssl certificates, but I'm not sure if that
    was one of the changes that mattered. I saw some documentation that said
    they should go there, instead of where I had them before (which, funny
    enough, I also found in documentation). I've also changed it to do all
    of the reconfiguring when the container is launched, not when it builds.
    I might change it back, I liked how it was before. I added configuration
    for the ssh port, but ssh still isn't working. I am currently working on
    setting up the ssh daemon in the gitlab container with runit. I've also
    added some more resources to the dockerfile.

--- 01_dockerfiles/ex03/gitlab.rb
+++ 01_dockerfiles/ex03/gitlab.rb
@@ -1,5 +1,6 @@
+external_url "https://192.168.99.107"
 letsencrypt['enable'] = false
 nginx['redirect_http_to_https'] = true
-nginx['ssl_certificate'] = "/etc/gitlab/ssl/gitlab.abarnett.com.crt"
-nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/gitlab.abarnett.com.key"
-external_url "https://gitlab.abarnett.com"
+nginx['ssl_certificate'] = "/etc/gitlab/trusted-certs/gitlab.abarnett.com.crt"
+nginx['ssl_certificate_key'] = "/etc/gitlab/trusted-certs/gitlab.abarnett.com.key"
+gitlab_rails['gitlab_shell_ssh_port'] = 8022
